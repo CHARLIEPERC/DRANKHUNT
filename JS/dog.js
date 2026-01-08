963bb30ec5b76cbc0cc7b3b2299f29ff9a9cf554
CHARLIEPERC <screwstonretail@gmail.com>
2026-01-08 13:11:38 -0600

    Delete JS/dog.js

--- JS/dog.js
+++
@@ -1,9 +0,0 @@
-// JS/Dog.js - stub so the game doesn't crash if Dog is referenced
-class Dog {
-  constructor() {}
-  launchWalkoutAnimation() {}
-  showDogWithKilledDucks() {}
-  liftGroundToDog() { return null; }
-  computeGroundBaseline() { return null; }
-}
-window.Dog = Dog;
